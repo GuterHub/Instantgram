c1da6b03b10bd76c1af0fbd37afa71af66a836f6
Piotr <guter.poczta@gmail.com>
2018-01-12 13:22:00 +0100

    widok bazowy

--- Instantgram/Instantgram/urls.py
+++ Instantgram/Instantgram/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from photoalbum.views import MainView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', MainView.as_view()),
+
 ]
--- Instantgram/photoalbum/views.py
+++ Instantgram/photoalbum/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+#from .models import Participant, Expense, ParticipantExpense
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
+#from django.urls import reverse
+#from django.shortcuts import get_object_or_404, reverse
 
-# Create your views here.
+#from .forms import LoginForm, RegisterForm, AddParticipantForm
+#from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+#from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.mixins import PermissionRequiredMixin
+
+from django.views import View
+
+class MainView(View):
+
+    def get(self, request):
+        return render(request, "main.html")
